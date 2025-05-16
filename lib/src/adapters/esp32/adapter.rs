1773bfb9e6d86b6842980a4edae3036f73802b35
Emilio Salvatore Gennaro Costanza <e.s.gennarocostanza@student.tudelft.nl>
2025-05-16 16:34:03 +0200

    ESP32 Adapter: basic structure

---
+++ lib/src/adapters/esp32/adapter.rs
@@ -0,0 +1,4 @@
+pub struct ESP32Adapter {
+    /// Whether to apply scaling to CSI data based on RSSI, noise, and AGC.
+    scale_csi: bool,
+}
---
+++ lib/src/adapters/esp32/mod.rs
@@ -0,0 +1,12 @@
+//! ESP32 Wi-Fi Module
+//!
+//! This module provides support for parsing and interacting with ESP32 CSI data,
+//!
+//! - adapter: Contains the logic for interfacing with ESP32 devices.
+mod adapter;
+
+/// Re-export of the main adapter interface for ESP32 devices.
+///
+/// This allows external crates/modules to access the adapter functionality
+/// via adapters::esp32::ESP32Adapter instead of the longer path adapters::esp32::adapter::ESP32Adapter.
+pub use crate::adapters::esp32::adapter::ESP32Adapter;
