9fcc939255615eaafe3f071e2b50b782765a3b17
Emilio Salvatore Gennaro Costanza <e.s.gennarocostanza@student.tudelft.nl>
2025-05-20 10:35:15 +0200

    CARGO: Format

--- sensei/src/cli.rs
+++ sensei/src/cli.rs
@@ -95,7 +95,7 @@ pub struct VisualiserSubcommandArgs {
     /// server port (default: 6969)
     #[argh(option, default = "String::from(\"127.0.0.1:6969\")")]
     pub target: String,
-    
+
     /// height of the eventual window
     #[argh(option, default = "default_height()")]
     pub height: usize,
