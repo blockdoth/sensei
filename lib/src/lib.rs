0553bdcbed189fe6d6f4234958a81d4ddf99039d
Ilia Ilinsky <I.S.Ilinsky-1@student.tudelft.nl>
2025-05-21 16:42:15 +0200

    Library: updated errorss and library documentation

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,13 @@
+//!
+//!Sensei library
+//!
+//! Contains utility functions, structs and enums. 
+//! The network implements the tcp logic
+//! Device handling is implemented through adapters, sources and sinks, 
+//! All of them are commbined in the handler pipeline
+//! Usefull errors are also made available
+//!
+
 use crate::errors::TaskError;
 use async_trait::async_trait;
 
@@ -10,8 +20,9 @@ pub mod network;
 pub mod sinks;
 pub mod sources;
 
-// Trait to create an instance from a configuration, needs to be implemented for all configurable things
-// Like sources, controllers, adapters and sinks
+/// Trait to create an instance from a configuration, needs to be implemented for all configurable things
+/// Like sources, controllers, adapters and sinks
+/// Very useful for initialization from Yaml files
 #[async_trait]
 pub trait FromConfig<C> {
     async fn from_config(config: C) -> Result<Box<Self>, TaskError>;
