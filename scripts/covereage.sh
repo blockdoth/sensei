efd1d7ff5feb4bb87f75973c6c594cfb2c7dc96e
Joeri Geuzinge <r.j.geuzinge@student.tudelft.nl>
2025-06-02 12:59:34 +0200

    Code coverage tooling

---
+++ scripts/covereage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p target/llvm-cov/lcov
+
+lcov_dir=target/llvm-cov/lcov
+html_dir=target/llvm-cov/html
+
+echo "Generating machine readable test report"
+cargo llvm-cov --lcov --output-path $lcov_dir/lcov.info --quiet  && # machine readable format
+echo "Generating human radable test report" &&
+cargo llvm-cov --html --output-dir $html_dir --quiet                # human readable format
+
+
+echo "The machine readable LCOV report is stored at $lcov_dir. The human readable report is stored at $html_dir/index.html."
