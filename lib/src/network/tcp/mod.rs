055ffcd74ada0b25313df3cb4ccd0de9295a4d2d
Joeri Geuzinge <r.j.geuzinge@student.tudelft.nl>
2025-06-14 00:40:17 +0200

    Added documentation for handle_send and handle_recv that mentiones the blocking issue.

--- lib/src/network/tcp/mod.rs
+++ lib/src/network/tcp/mod.rs
@@ -2,7 +2,7 @@ use std::net::SocketAddr;
 use std::time::Duration;
 
 use async_trait::async_trait;
-use log::{debug, error, info, trace};
+use log::{debug, error, info};
 use serde::Deserialize;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
@@ -20,67 +20,59 @@ pub const MAX_MESSAGE_LENGTH: usize = 4096;
 pub const CONNECTION_TIMEOUT: Duration = Duration::from_secs(10);
 
 pub async fn read_message(read_stream: &mut OwnedReadHalf, buffer: &mut [u8]) -> Result<Option<RpcMessage>, NetworkError> {
-    let res = tokio::time::timeout(CONNECTION_TIMEOUT, async move {
-        let mut length_buffer = [0; 4];
+    let mut length_buffer = [0; 4];
 
-        let msg_length = match read_stream.read_exact(&mut length_buffer).await {
-            Ok(_) => Ok(u32::from_be_bytes(length_buffer) as usize),
-            Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => {
-                info!("Stream closed by peer.");
-                Err(NetworkError::Closed)
-            }
-            Err(e) => Err(NetworkError::Io(e)), //TODO: better error handling
-        }?;
-
-        debug!("Received message of length {msg_length}");
-
-        if msg_length > MAX_MESSAGE_LENGTH {
-            error!("Message of length {msg_length} is to long, max message length: {MAX_MESSAGE_LENGTH}");
-            return Err(NetworkError::Serialization);
+    let msg_length = match read_stream.read_exact(&mut length_buffer).await {
+        Ok(_) => Ok(u32::from_be_bytes(length_buffer) as usize),
+        Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => {
+            info!("Stream closed by peer.");
+            Err(NetworkError::Closed)
         }
-        if msg_length == 0 {
-            // todo!("handle keep alive packets");
-            return Ok(None);
-        }
+        Err(e) => Err(NetworkError::Io(e)), //TODO: better error handling
+    }?;
 
-        let mut bytes_read: usize = 0;
-        while bytes_read < msg_length {
-            let n_read: usize = read_stream.read(&mut buffer[bytes_read..msg_length]).await?;
-            debug!("Read {n_read} bytes from buffer");
-            if n_read == 0 {
-                error!("stream closed before all bytes were read ({bytes_read}/{msg_length})");
-                return Err(NetworkError::Closed);
-            }
-            bytes_read += n_read;
+    debug!("Received message of length {msg_length}");
+
+    if msg_length > MAX_MESSAGE_LENGTH {
+        error!("Message of length {msg_length} is to long, max message length: {MAX_MESSAGE_LENGTH}");
+        return Err(NetworkError::Serialization);
+    }
+    if msg_length == 0 {
+        // todo!("handle keep alive packets");
+        return Ok(None);
+    }
+
+    let mut bytes_read: usize = 0;
+    while bytes_read < msg_length {
+        let n_read: usize = read_stream.read(&mut buffer[bytes_read..msg_length]).await?;
+        debug!("Read {n_read} bytes from buffer");
+        if n_read == 0 {
+            error!("stream closed before all bytes were read ({bytes_read}/{msg_length})");
+            return Err(NetworkError::Closed);
         }
-        //TODO fix error handling
-        Ok(Some(deserialize_rpc_message(&buffer[..msg_length])?))
-    })
-    .await?;
-    trace!("{res:#?}");
-    res
+        bytes_read += n_read;
+    }
+    //TODO fix error handling
+    Ok(Some(deserialize_rpc_message(&buffer[..msg_length])?))
 }
 
 pub async fn send_message(stream: &mut OwnedWriteHalf, msg: RpcMessageKind) -> Result<(), NetworkError> {
-    tokio::time::timeout(CONNECTION_TIMEOUT, async move {
-        let msg_wrapped = make_msg(&stream, msg);
-        let msg_serialized = serialize_rpc_message(msg_wrapped)?;
-        let msg_length: u32 = msg_serialized.len().try_into().unwrap();
+    let msg_wrapped = make_msg(&stream, msg);
+    let msg_serialized = serialize_rpc_message(msg_wrapped)?;
+    let msg_length: u32 = msg_serialized.len().try_into().unwrap();
 
-        if msg_length as usize > MAX_MESSAGE_LENGTH {
-            error!("Message of length {msg_length} is to long, max message length: {MAX_MESSAGE_LENGTH}");
-            return Err(NetworkError::Serialization);
-        }
-        let msg_length_serialized = msg_length.to_be_bytes();
+    if msg_length as usize > MAX_MESSAGE_LENGTH {
+        error!("Message of length {msg_length} is to long, max message length: {MAX_MESSAGE_LENGTH}");
+        return Err(NetworkError::Serialization);
+    }
+    let msg_length_serialized = msg_length.to_be_bytes();
 
-        debug!("Sending message of length {msg_length:?}");
+    debug!("Sending message of length {msg_length:?}");
 
-        stream.write_all(&msg_length_serialized).await?;
-        stream.write_all(&msg_serialized).await?;
-        stream.flush().await?;
-        Ok(())
-    })
-    .await?
+    stream.write_all(&msg_length_serialized).await?;
+    stream.write_all(&msg_serialized).await?;
+    stream.flush().await?;
+    Ok(())
 }
 
 // TODO better error handling
@@ -93,9 +85,57 @@ fn deserialize_rpc_message(buf: &[u8]) -> Result<RpcMessage, Box<NetworkError>> 
 }
 
 #[async_trait]
+/// Trait for handling TCP connection events in an asynchronous network context.
+///
+/// Implementors of this trait are responsible for processing received messages
+/// and managing outgoing data over a TCP connection. All methods are asynchronous
+/// and require implementors to be both `Send` and `Sync`.
+///
+/// # Methods
+///
+/// - `handle_recv`: Handles an incoming `RpcMessage` and may send commands via the provided channel.
+/// - `handle_send`: Processes outgoing messages and data, sending them over the provided write stream.
 pub trait ConnectionHandler: Send + Sync {
+    /// Handles an incoming `RpcMessage` received over the TCP connection.
+    ///
+    /// This asynchronous method is called whenever a new message is received from the peer.
+    /// Implementors should process the message and may send commands or updates through the
+    /// provided `send_commands_channel`.
+    /// While synchronous code inside this function is executed the tokio worker is "blocked".
+    /// I.e. Tokio does not insert yield points automagically, and busy looping inside handle_recv blocks the worder
+    /// for the duration of the connection. Be sure to insert yield points in large blocks of code.
+    /// `tokio::task::consume_budget().await;` can be used in long blocks of synchronous code.
+    ///
+    /// # Arguments
+    ///
+    /// * `request` - The received `RpcMessage` to be handled.
+    /// * `send_commands_channel` - A `watch::Sender<ChannelMsg>` for sending commands or updates
+    ///   to other parts of the system.
+    ///
+    /// # Returns
+    ///
+    /// Returns `Ok(())` if the message was handled successfully, or a `NetworkError` if an error occurred.
     async fn handle_recv(&self, request: RpcMessage, send_commands_channel: watch::Sender<ChannelMsg>) -> Result<(), NetworkError>;
 
+    /// Handles outgoing messages and data to be sent over the TCP connection.
+    ///
+    /// This asynchronous method is responsible for processing outgoing commands and data,
+    /// and sending them over the provided write stream to the peer. It listens for new
+    /// commands via the `recv_commands_channel` and for data messages via the `recv_data_channel`.
+    /// While synchronous code inside this function is executed the tokio worker is "blocked".
+    /// I.e. Tokio does not insert yield points automagically, and busy looping inside handle_recv blocks the worder
+    /// for the duration of the connection. Be sure to insert yield points in large blocks of code.
+    /// `tokio::task::consume_budget().await;` can be used in long blocks of synchronous code.
+    ///
+    /// # Arguments
+    ///
+    /// * `recv_commands_channel` - A `watch::Receiver<ChannelMsg>` for receiving outgoing commands.
+    /// * `recv_data_channel` - A `broadcast::Receiver<(DataMsg, DeviceId)>` for receiving outgoing data messages.
+    /// * `send_stream` - An `OwnedWriteHalf` representing the writable half of the TCP stream.
+    ///
+    /// # Returns
+    ///
+    /// Returns `Ok(())` if all messages were sent successfully, or a `NetworkError` if an error occurred.
     async fn handle_send(
         &self,
         mut recv_commands_channel: watch::Receiver<ChannelMsg>,
@@ -127,6 +167,7 @@ pub enum HostChannel {
     ListenSubscribe { addr: SocketAddr },
     ListenUnsubscribe { addr: SocketAddr },
     Configure { device_id: DeviceId, cfg_type: CfgType },
+    Pong,
 }
 
 #[derive(Debug, Clone, Deserialize)]
